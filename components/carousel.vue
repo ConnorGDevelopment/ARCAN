42149ff9fd782cbc74ceba8f1fe060e9af552f53
ConnorGDevelopment <connorgdevelopment@gmail.com>
2020-06-04 09:04:10 -0400

    Hero Carousel

--- components/carousel.vue
+++ components/carousel.vue
@@ -1,10 +1,28 @@
 <template>
   <v-row>
-    <v-col>
-      Test
+    <v-col
+      v-for="image in carousel"
+      :id="image.title"
+      :key="image.title"
+      class="pa-0 carousel"
+      :style="{width : image.width}"
+      @mouseover="expand(image.index)"
+      @mouseleave="shrink"
+    >
+      <v-img
+        class="backgroundImage"
+        :src="image.link"
+      />
+      </v-hover>
     </v-col>
   </v-row>
 </template>
+
+<style>
+.carousel {
+  min-height: calc(100vh - 64px);
+}
+</style>
 
 <script>
   export default {
@@ -13,26 +31,48 @@
         carousel: [
           {
             title: 'events',
+            index: 0,
             link: '/img/events.png',
+            width: '20%',
           },
           {
             title: 'protesting',
+            index: 1,
             link: '/img/protesting.png',
+            width: '20%',
           },
           {
             title: 'guides',
+            index: 2,
             link: '/img/guides.png',
+            width: '20%',
           },
           {
             title: 'community',
+            index: 3,
             link: '/img/community.png',
+            width: '20%',
           },
           {
             title: 'donate',
+            index: 4,
             link: '/img/donate.png',
+            width: '20%',
           },
         ],
       }
+    },
+    methods: {
+      expand (carouselIndex) {
+        for (const item of this.carousel) {
+          item.width = item.index === carouselIndex ? '80%' : '5%'
+        }
+      },
+      shrink (carouselIndex) {
+        for (const item of this.carousel) {
+          item.width = '0%'
+        }
+      },
     },
   }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -11,4 +11,9 @@
 .v-card__text, .v-card__title, .v-card__subtitle {
   word-break: normal !important;
 }
+.backgroundImage {
+  background-size: cover;
+  background-position: center;
+  height: calc(100vh - 64px);
+}
 </style>
